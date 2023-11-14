87ff7857c8fb0729ed43d06ff9ced8c422a1c4b6
KK <2846016855@qq.com>
2023-11-14 21:33:07 +0800

    Helloworld

---
+++ day01-helloworld/src/com/hk/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.hk.helloworld;
+
+/**
+ * @author Fcun
+ * @data 2023/11/14 --- 21:29
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
