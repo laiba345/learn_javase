2d015a2a192912c1ffa7a5f30351958d5a189b97
KK <2846016855@qq.com>
2024-01-01 13:14:47 +0800

    JDBC之工具类的使用;

--- learn_jdbc/src/com/jdbc/Utils/JDBCUtils_Use.java
+++ learn_jdbc/src/com/jdbc/Utils/JDBCUtils_Use.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -39,6 +40,41 @@ public class JDBCUtils_Use {
         } finally {
             // 关闭资源
             JDBCUtils.close(null, preparedStatement, connection);
+        }
+    }
+
+    @Test
+    public void testSelect() { // Select
+        // 1. 得到连接; 都是可以很方便的用于扩大作用域 ;
+        Connection connection = null;
+
+        // 2. 组织一个sql
+        // String sql = "select * from actor";
+        String sql = "select * from actor where id = ?"; // 也可以使用占位符来操作
+        PreparedStatement preparedStatement = null;
+        ResultSet set = null;
+        // 3. 创建PreparedStatement对象
+        try {
+            connection = JDBCUtils.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            // 如果使用了占位符就需要使用此处
+            preparedStatement.setInt(1, 4); // 给占位符赋上相应的值;
+            // 执行
+            set = preparedStatement.executeQuery();
+            // 遍历该结果集
+            while(set.next()) {
+                int id = set.getInt("id");
+                String name = set.getString("name");
+                String sex = set.getString("sex");
+                String borndate = set.getString("borndate");
+                String phone = set.getString("phone");
+                System.out.println(id + "\t" + name + "\t" + sex + "\t" + borndate + "\t" + phone);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // 关闭资源
+            JDBCUtils.close(set, preparedStatement, connection);
         }
     }
 }
